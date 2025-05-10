91675db8406e9c3f2c77581d0ef18c0769a27d43
JohnnyASan <johnanthonysanabria@gmail.com>
2025-05-09 22:41:50 -0600

    fixed send msg bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'cms';
-  loadedFeature: string = '';
+  loadedFeature: string = 'messages';
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -12,9 +12,6 @@ export class MessageListComponent {
     new Message('1', 'Message 1', 'This is the first message.', 'User A'),
     new Message('2', 'Message 2', 'This is the second message.', 'User B'),
     new Message('3', 'Message 3', 'This is the third message.', 'User C'),
-    new Message('4', 'Message 4', 'This is the fourth message.', 'User D'),
-    new Message('5', 'Message 5', 'This is the fifth message.', 'User E'),
-    new Message('6', 'Message 6', 'This is the sixth message.', 'User F'),
   ];
 
   onMessageSent(message: Message) {
